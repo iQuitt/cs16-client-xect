591f51adbad55036828912fe6413e25eb6fbe75b
hasandramali <93338836+hasandramali@users.noreply.github.com>
2024-11-05 00:24:52 +0200

    Update Application.mk

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -7,7 +7,7 @@ APPLICATIONMK_PATH = $(call my-dir)
 
 XASH3D_CONFIG := $(APPLICATIONMK_PATH)/bs_config.mk
 
-APP_ABI := x86 armeabi-v7a-hard
+APP_ABI := armeabi-v7a-hard
 APP_MODULES := client server
 APP_PLATFORM := android-9
 APP_STL := gnustl_static
